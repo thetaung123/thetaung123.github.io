bcbd7a211249b6407fc44f0545eca12ecc25cdb1
Thet Aung <zarnee96@gmail.com>
2019-11-25 12:11:13 +0630

    adding new nav bar

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,21 +1,41 @@
 import React from 'react';
-import './Nav.css'
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
-const Navigation = ({onRouteChange, isSignedIn}) => {
-    if(isSignedIn){
-        return (<nav className='custom_nav'>
-            <p className="custom_link" onClick={() => onRouteChange('SignIn')}>Sign
-                Out</p>
-        </nav>);
-    }else {
-        return (
-            <nav className='custom_nav'>
-                <p className="custom_link" onClick={() => onRouteChange('SignIn')}>Sign
-                    In</p>
-                <p className="custom_link"
-                   onClick={() => onRouteChange('Register')}>Register</p>
-            </nav>
-        );
-    }
-};
-export default Navigation;
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+}));
+
+export default function ButtonAppBar({onRouteChange,isSignedIn}) {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" className={classes.title}>
+                        {isSignedIn===true ? 'User Profile' : 'Yes Doc ' }
+                    </Typography>
+                    {isSignedIn===false ? <Button color="inherit" onClick={() => onRouteChange('SignIn')}>SignIn</Button>
+                        :<Button color="inherit" onClick={() => onRouteChange('SignIn')}>SignOut</Button>}
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+}
